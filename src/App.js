aa41d74c1edeed9155d1125af10d5ac9e49f6e58
Krishna babu <indiakrish@gmail.com>
2022-05-23 15:18:50 +0530

    firebase email and password login

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import ProductList from "./components/ProuductList";
 import CheckoutAd from "./components/CheckoutAd";
 import ShoppingBasket from "./components/ShoppingBasket";
 import React from "react";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
-      <Header/>
       <Routes>
         <Route exact path='/' element={
           <React.Fragment>
+            <Header/>
             <Banner/>
             <ProductList/>
           </React.Fragment>
         }/>
         <Route exact path='/checkout' element={
           <React.Fragment>
+            <Header/>
             <CheckoutAd/>
             <ShoppingBasket/>
           </React.Fragment>
+        } />
+        <Route exact path='/login' element={
+          <Login />
         } />
       </Routes>
     </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ import { useStateValue } from "./StateProvider";
 const Header = () => {
   const { myReducer } = useStateValue();
   const [ data ] = myReducer;
-  
+  console.log(data.authInfo)
   return(
     <header className="header">
       <div className="header__logo">
@@ -23,10 +23,12 @@ const Header = () => {
         <SearchIcon fontSize="small" className="header__searchIcon"/>
       </div>
       <div className="header__nav">
-        <div className="header__nav__user">
-          <span className="header__nav__lineOne">Hello Guest</span>
-          <span className="header__nav__lineTwo">Sign In</span>
-        </div>
+      <Link to='/login'>
+          <div className="header__nav__user">
+            <span className="header__nav__lineOne">{data.authInfo.user ? data.authInfo.user.email:'Hello Guest'}</span>
+            <span className="header__nav__lineTwo">Sign In</span>
+          </div>
+        </Link>
         <div className="header__nav__itemBasket">
           <Link to="/checkout">
             <ShoppingCartIcon fontSize="small"/>&nbsp;{data.cartList?.length}&nbsp;Cart
